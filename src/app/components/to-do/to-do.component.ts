cfa6382257b355a959f1704a074b78d6522cfcc7
mariam <mariam.mohamed2691319@gmail.com>
2025-03-05 22:36:04 +0200

    Updated To-Do component and index.html

--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -33,11 +33,17 @@ constructor(private fb : FormBuilder){}
 
   ngOnInit(): void {
     this.toDoForm = this.fb.group({
-      item:['' , Validators.required],
-      description:['' , Validators.required],
+      item:['' , [Validators.required , Validators.minLength(3)]],
+      description:['' , [Validators.required  , Validators.minLength(10) , Validators.maxLength(30)]],
       date:['', Validators.required],
       priority:['high', Validators.required],
       } )
+  }
+
+  // Helper function to check form control errors
+  hasError(controlName: string, errorName: string) {
+    return this.toDoForm.get(controlName)?.hasError(errorName) &&
+           this.toDoForm.get(controlName)?.touched;
   }
   addTask(){
     this.tasks.push({
@@ -50,23 +56,40 @@ constructor(private fb : FormBuilder){}
     this.toDoForm.reset();
   }
 
-
+//update task
   updateTask(){
     this.tasks[this.updatedIndex].title = this.toDoForm.value.item;
+    this.tasks[this.updatedIndex].deadline = this.toDoForm.value.date;
+    this.tasks[this.updatedIndex].priority = this.toDoForm.value.priority;
+    this.tasks[this.updatedIndex].description = this.toDoForm.value.description;
     this.tasks[this.updatedIndex].Completed = false;
     this.toDoForm.reset();
     this.updatedIndex = undefined;
     this.isEditEnabled = false;
   }
-  deleteTask(taskId:number){
-    this.tasks.splice(taskId,1)
-  }
+//edit task
   editTask(taskId: number , task:Task ){
     this.toDoForm.controls['item'].setValue(task.title);
+    this.toDoForm.controls['description'].setValue(task.description);
+    this.toDoForm.controls['priority'].setValue(task.priority);
+    this.toDoForm.controls['date'].setValue(task.deadline);
     this.updatedIndex = taskId;
     this.isEditEnabled = true;
   }
+//delete task
+  deleteTask(taskId:number){
+    this.tasks.splice(taskId,1)
+  }
 
+  deleteInprogressTask(taskId:number){
+    this.inProgress.splice(taskId,1)
+  }
+  deleteDoneTask(taskId:number){
+    this.done.splice(taskId,1)
+
+  }
+
+  //drop using Angular Matriel
   drop(event: CdkDragDrop<Task[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
